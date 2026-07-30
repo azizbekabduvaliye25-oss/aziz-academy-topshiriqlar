0be100b8a06698bef04c7c23843d01ca412b2b0d
azizbekabduvaliye25-oss <azizbekabduvaliye25@gmail.com>
2026-07-30 10:01:57 +0300

    Add: Profil kartasi [azizbekabduvaliye25-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,12 @@
+ism = "Aziz"
+familiya = "Karimov"
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+
+print("=== PROFIL ===")
+print("Ism: Aziz")
+print("Familiya: Karimov")
+print("Kurs: Python")
+print("Daraja: Boshlang'ich")
+print("Shahar: Toshkent")
